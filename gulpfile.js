15a23e17d05c3494f87504902ca8f5ab9296786b
jloh <github@jloh.co>
2017-05-08 23:14:50 +1000

    Add gulp pipeline for headers file
    This is used so that the CSS file link is always up to date

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,14 @@ var gulp         = require("gulp"),
     rename       = require('gulp-rename');
     autoprefixer = require("gulp-autoprefixer"),
     hash         = require("gulp-hash"),
-    del          = require("del")
+    del          = require("del"),
+    template     = require('gulp-template')
 
 // Compile LESS files to CSS
-gulp.task("less", function () {
+gulp.task("less", function() {
   del(["static/css/**/*"])
-  gulp.src("src/less/main.less")
+  // A return is needed here so that gulp waits for this task to write its JSON hash
+  return gulp.src("src/less/main.less")
     .pipe(less({outputStyle : "compressed"}))
     .pipe(autoprefixer({browsers : ["last 20 versions"]}))
     .pipe(csscomb())
@@ -26,10 +28,21 @@ gulp.task("less", function () {
     .pipe(gulp.dest("data/css"))
 })
 
+// Generate our _headers file
+// We only need to do this during actual deployment since its just used by netlify
+// Must be done after the 'less' function so that our JSON exists
+gulp.task("headers", ['less'], function() {
+  // Load our JSON file
+  var cssjson = require('./data/css/hash.json')
+  gulp.src('src/_headers')
+    .pipe(template({maincss: cssjson['main.css']}))
+    .pipe(gulp.dest('static'))
+})
+
 // Watch asset folder for changes
 gulp.task("watch", ["less"], function () {
   gulp.watch(["src/less/*.less", "src/less/**/*.less"], ["less"])
 })
 
 // Build task
-gulp.task('default', ['less'])
+gulp.task('default', ['less', 'headers'])
